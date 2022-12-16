c5abf52fe8efddab41a5c63a6338bd3fcbc90601
Gawain Bracy II <bracy.gawain2014@gmail.com>
2022-12-16 13:22:42 -0500

    env update
    Signed-off-by: Gawain Bracy II <bracy.gawain2014@gmail.com>

--- src/pages/api/siwe/siwe.ts
+++ src/pages/api/siwe/siwe.ts
@@ -2,5 +2,5 @@ import { configureSIWE } from "connectkit-next-siwe";
 
 export const siwe = configureSIWE({
   apiRoutePrefix: "/api/siwe",
-  session: { password: "3aCJxxKaMGahNmK.gpWJkb-@Cvn*xDRm" }
+  session: { password: process.env.SESSION_SECRET }
 });
