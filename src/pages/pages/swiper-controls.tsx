0f1007c7731d234f246ddb90dbdc5acf52aed8d3
Gawain Bracy II <bracy.gawain2014@gmail.com>
2022-12-28 20:06:54 -0500

    update config
    Added new path routings
    
    Signed-off-by: Gawain Bracy II <bracy.gawain2014@gmail.com>

--- src/pages/pages/swiper-controls.tsx
+++ src/pages/pages/swiper-controls.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../components/Navbar/header'
 import SwiperControls from '../../components/pages/SwiperControls'
 
-export default function Home() {
+export default function Swiper() {
   return (
       <>
         <Header>
