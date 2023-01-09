931539ac1133c08c602ee84fb0f8ac51ee2b6a3b
Gawain Bracy II <bracy.gawain2014@gmail.com>
2023-01-09 09:58:58 -0500

    Add Sales Email
    Signed-off-by: Gawain Bracy II <bracy.gawain2014@gmail.com>

--- src/components/Navbar/header.tsx
+++ src/components/Navbar/header.tsx
@@ -861,6 +861,7 @@ const Features = (props: any) => {
             </LinkOverlay>
           </Section>
         </LinkBox>
+
         <LinkBox color={tcl}>
           <Section
             title="DAO Proposals"
@@ -983,7 +984,9 @@ const Features = (props: any) => {
                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                 />
               </chakra.svg>
+              <LinkOverlay href="mailto:sales@creativeplatform.xyz" target={'_blank'}>
               <chakra.span ml={3}>Contact Sales</chakra.span>
+              </LinkOverlay>
             </LinkBox>
           </Box>
         </Stack>
